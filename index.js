c9a0dd57eaced242ed761783fe4b40c528c8233f
ashish <ashishroy7652@gmail.com>
2023-01-23 03:02:52 +0530

    added nodemon

--- index.js
+++ index.js
@@ -8,48 +8,40 @@
 //  step 4.a : set up our server
 const express = require("express");
 const app = express();
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 
-const port = 8000;
+const port = 8001;
 
 // step 8 . express layouts
-const expressLayouts = require('express-ejs-layouts') ; 
-const db = require('./config/mongoose') ; 
-
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
 
 // extract style and script from subpages into layouts
-app.set('layout extractStyles' , true ) ; 
-app.set('layout extractScripts' , true ) ; 
-
-
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // step 9 . use of static file
-app.use(express.static('./assets')) ; 
+app.use(express.static("./assets"));
 app.use(bodyParser.json());
 
 // it helps us to get data from form in req.body
 app.use(express.urlencoded({ extended: true }));
 
-//step 8 . use layouts and footer and header 
-app.use(expressLayouts) ; 
+//step 8 . use layouts and footer and header
+app.use(expressLayouts);
 
-
-// step5.2 go to routes folder make a index.js file for all the routes and use it in entry js 
+// step5.2 go to routes folder make a index.js file for all the routes and use it in entry js
 // use of router express
-app.use('/' , require('./routes')) ;  
+app.use("/", require("./routes"));
 
-
-
-//step 7 . set up our view engine 
-app.set('view engine' , 'ejs') ; 
-app.set('views' , './views') ; 
-
-
+//step 7 . set up our view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 //  step 4.b server listen part
 app.listen(port, function (err) {
   if (err) {
-    // interpolation using backtick 
+    // interpolation using backtick
     console.log(`Error in running the server : ${err}`);
   }
   console.log(`Server is running on port : ${port}`);
